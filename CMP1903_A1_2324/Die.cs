3c79e11493c927dbff591db2ea9d5f7dff1181a6
Sammy226 <samuelcmwright@live.co.uk>
2024-02-22 23:53:38 +0000

    created first draft
    created three dice variables and a total number variable which adds up their values

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -8,6 +8,16 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
+        static void Main(string[] args)
+        {
+            int dice = 0;
+
+            Random random = new Random();
+            int diceroll = random.Next(1, 7);
+            dice = dice + diceroll;
+
+            Console.WriteLine(dice);
+        }
         /*
          * The Die class should contain one property to hold the current die value,
          * and one method that rolls the die, returns and integer and takes no parameters.
--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace CMP1903_A1_2324
 {
     internal class Game
     {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int dice1 = random.Next(1, 7);
+            int dice2 = random.Next(1, 7);
+            int dice3 = random.Next(1, 7);
+            int dicetotal = dice1 + dice2 + dice3;
+
+            Console.WriteLine("The first dice rolled a " + dice1);
+            Console.WriteLine("The second dice rolled a " + dice2);
+            Console.WriteLine("The third dice rolled a " + dice3);
+            Console.WriteLine("The total of value of the dice is " + dicetotal);
+        }
         /*
          * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
          *
