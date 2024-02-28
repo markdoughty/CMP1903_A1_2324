6225857c4c6d920047c09e7ba2639e4a16aebdd4
llama2474 <wiktorledzion@gmail.com>
2024-02-28 15:32:39 +0000

    Update Program.cs

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -8,16 +8,6 @@ namespace CMP1903_A1_2324
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Game game = new Game(); //Creates a new game object
-            game.Play(); //Calls the play function for the game object
-            
-            Testing testing = new Testing();//Creates a new game object
-            testing.TestGame(); //Calls the test game function on thr testing object
-
-            Console.ReadLine();
-
-        }
+       
     }
 }
