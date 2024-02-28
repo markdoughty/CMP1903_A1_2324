e10859ed3f8da8f091d7bfa033f35359d4eeda79
Fnly23 <145765813+Fnly23@users.noreply.github.com>
2024-02-28 11:43:33 +0000

    updated, for loop working

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,26 @@ namespace CMP1903_A1_2324
     {
         /*
          * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
+         
+         *
+         *
          *
          * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
          * rolls could be continous, and the totals and other statistics could be summarised for example.
          */
+
+
+
+       // Random rng = new Random();
+       // int dice1 = rng.Next(1, 7);
+       // int dice2 = rng.Next(1, 7);
+       // int dice3 = rng.Next(1, 7);
+
+
+
+
+
+
 
         //Methods
 
--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -12,8 +12,36 @@ namespace CMP1903_A1_2324
         {
             /*
              * Create a Game object and call its methods.
+            
+             
+             
              * Create a Testing object to verify the output and operation of the other classes.
              */
+            Console.WriteLine("Enter the number of dice you want to roll: ");
+            // user input is converted to an interger to be able to add.
+            int dice_user_input = Convert.ToInt32(Console.ReadLine());
+          
+            Random rng = new Random();
+           
+            int Random_Number = rng.Next(dice_user_input);  
+           // for loop which makes it so it prints out the statement and the number for the amount of numbers that the user has asked it to print out,
+            for (int j = 0; j < dice_user_input; j++)
+            {
+                
+                Console.WriteLine("the numbers the dice rolled: " + rng.Next(dice_user_input));
+            }
+            int sum_of_dice;
+            sum_of_dice = Random_Number + (Random_Number);
+            
+            //int sum_of_dice = ;
+            Console.WriteLine("The number of all of the dice added together is: {0} " ,(sum_of_dice));
+            
+            
+            
+            
+            Console.ReadKey();
+        
         }
+
     }
 }
