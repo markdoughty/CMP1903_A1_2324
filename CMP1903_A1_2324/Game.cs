44525f959c8825c592050d284248fa8d3322f84e
llama2474 <wiktorledzion@gmail.com>
2024-02-28 15:35:12 +0000

    Uploaded Code

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -8,6 +8,25 @@ namespace CMP1903_A1_2324
 {
     internal class Game
     {
+        public int FinalSum //Public variable that sets and gets the private variable sum
+        { 
+            get { return _sum; }
+            set {  _sum = value; }
+        }
+        private int _sum = 0; //Private variable sum
+        public void Play() //Main method for the class
+        {
+            Dice dice = new Dice(); //Creates a new dice object
+            int diceVal = 0;
+            
+            for (int i = 0;i<=2;i++) //Loops 3 times to roll the dice 3 times and add the number roll to the sum
+            {
+                diceVal = dice.RollDie(); //Calls the roll die method on the dice objects and the returning integer is set as DieVal
+                _sum = _sum + diceVal;
+            }
+            FinalSum = _sum;
+            Console.WriteLine($"The sum of the three dice rolls is {_sum}");
+        }
 
     }
 }
--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -10,6 +10,18 @@ namespace CMP1903_A1_2324
     internal class Testing
     {
 
-      
+        public void TestGame() 
+        {
+            Console.WriteLine("Testing Game Object");
+            Game game = new Game(); //Creates a new Game object called game
+            game.Play();
+            Debug.Assert(2<game.FinalSum && game.FinalSum < 25 ); //Checks that the final sum is possilbe one 3 6 sided dice
+            
+            Console.WriteLine("Testing Dice Object");
+            Dice dice = new Dice(); //Creates a new Dice object called dice
+            dice.RollDie();
+            Debug.Assert(0<dice.FinalDie&&dice.FinalDie<7); //Checks that the dice rolls between 1 and 6
+        }
+
     }
 }
