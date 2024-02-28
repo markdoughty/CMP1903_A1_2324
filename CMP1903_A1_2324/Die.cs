373576a1d1d7c9d0f162bd2a2f7dca3bab510b2f
magoed12 <107357062+magoed12@users.noreply.github.com>
2024-02-28 01:18:51 +0000

    Restored files

---
+++ CMP1903_A1_2324/Die.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903_A1_2324
+{
+    internal class Die
+    {
+        public write()
+        {
+            Console.Writeline("hello");
+        }
+    }
+}
