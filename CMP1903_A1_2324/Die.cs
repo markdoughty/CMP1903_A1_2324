8a2081a47c2d2b00531c7b049354ca3947781169
llama2474 <wiktorledzion@gmail.com>
2024-02-28 15:44:23 +0000

    Fix?

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -6,9 +6,22 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Dice 
+    internal class Die
     {
+<<<<<<< HEAD
        
+=======
+        /*
+         * The Die class should contain one property to hold the current die value,
+         * and one method that rolls the die, returns and integer and takes no parameters.
+         */
+
+        //Property
+
+
+        //Method
+
+>>>>>>> parent of da7253d (Finished Coding)
 
     }
 }
--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +8,18 @@ namespace CMP1903_A1_2324
 {
     internal class Testing
     {
+<<<<<<< HEAD
 
        
+=======
+        /*
+         * This class should test the Game and the Die class.
+         * Create a Game object, call the methods and compare their output to expected output.
+         * Create a Die object and call its method.
+         * Use debug.assert() to make the comparisons and tests.
+         */
+
+        //Method
+>>>>>>> parent of da7253d (Finished Coding)
     }
 }
