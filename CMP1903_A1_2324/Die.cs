99de3ef697219e721b67c626fc2cd5ed4f5e574b
magoed12 <107357062+magoed12@users.noreply.github.com>
2024-02-28 01:12:36 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -8,16 +8,9 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
-        /*
-         * The Die class should contain one property to hold the current die value,
-         * and one method that rolls the die, returns and integer and takes no parameters.
-         */
-
-        //Property
-
-
-        //Method
-
-
+        public write()
+        {
+            Console.Writeline("hello");
+        }
     }
 }
