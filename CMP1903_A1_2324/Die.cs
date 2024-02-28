fd81daccc7deb68a6181b3f0a06ef42935cd3e82
llama2474 <wiktorledzion@gmail.com>
2024-02-28 15:38:54 +0000

    Cleared Main Branch

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -8,22 +8,7 @@ namespace CMP1903_A1_2324
 {
     internal class Dice 
     {
-        public int FinalDie //Public variable encapsulates _diceVal
-        { 
-          get { return _diceVal; }
-          set { _diceVal = value; } 
-        }
-
-        private int _diceVal = 0;
-
-        Random random = new Random(); //Creates a new Random object
-        public int RollDie()
-        {
-            
-            _diceVal = random.Next(1,7); //Generates a random number between 1 and6
-            Console.WriteLine($"You rolled a {_diceVal}");
-            return _diceVal;
-        }
+       
 
     }
 }
