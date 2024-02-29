0474e3018ea535b74a30479ef661990a3e4bd8f3
naruto08060119487 <160050698+naruto08060119487@users.noreply.github.com>
2024-02-29 18:51:27 +0000

    Update Game.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -15,22 +15,24 @@ namespace CMP1903_A1_2324
          * rolls could be continous, and the totals and other statistics could be summarised for example.
          */
 
-        public void PLay()
+        public string PLay()
         {
-            Die die1 = new Die();
+            Die die1 = new Die(); //here three new Die are created
             Die die2 = new Die();
             Die die3 = new Die();
 
-            int roll1 = die1.Roll();
+            int roll1 = die1.Roll(); //and they're rolled here
             int roll2 = die2.Roll();
             int roll3 = die3.Roll();
 
-            int total = roll1 + roll2 + roll3;
-                
+            int total = roll1 + roll2 + roll3; //and they're totalled here
+            
             Console.WriteLine("die 1 is: " + roll1);
             Console.WriteLine("die 2 is: " + roll2);
             Console.WriteLine("die 3 is: " + roll3);
-            Console.WriteLine("total is: " + total);
+            Console.WriteLine("total is: " + total); //these blocks shows the output of the Die rolls
+                
+            return $"die 1 is: {roll1}\ndie 2 is: {roll2}\ndie 3 is: {roll3}\ntotal is: {total}"; //this returens the game class output which will be used in the testing class
         }
 
     }
