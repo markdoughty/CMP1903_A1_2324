ec21f06f70bf14fb6124c364a4f36a9599cef019
naruto08060119487 <160050698+naruto08060119487@users.noreply.github.com>
2024-02-29 18:48:17 +0000

    Update Die.cs
    this creates a die and stores the output

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -13,8 +13,7 @@ namespace CMP1903_A1_2324
          * and one method that rolls the die, returns and integer and takes no parameters.
          */
 
-        public int StoreValue { get; private set; }    
-
+        public int StoreValue { get; private set; } //to store the value gotten from rolling the dice
 
         public int Roll()
         {
@@ -22,7 +21,5 @@ namespace CMP1903_A1_2324
             StoreValue = random.Next(1, 7);
             return StoreValue;
         }
-
-
     }
 }
