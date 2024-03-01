4372c36687fb906b27e0bac1b8a14ca6d0ff6a49
JustinasUrb <justas1101@gmail.com>
2024-03-01 09:18:54 +0000

    Addded comments for Die.cs and Testing.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -4,19 +4,19 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
-        private int _dieValue;
-        private static Random rnd = new Random();
-        private static Testing test = new Testing();
+        private int _dieValue; //creates a private variable for _dieValue, so that it couldn't be used by other c# files linked.
+        private static Random rnd = new Random(); //creates a private and static random element, which is only going top be used in one area.
+        private static Testing test = new Testing(); //A new object for Testing is created here, so that tests can be made as soon as possible.
 
-        public int DieValue
+        public int DieValue //A public variation of _dieValue is created here, so that it can be used elsewhere.
         {
-            get { return _dieValue; }
+            get { return _dieValue; } //Returns a value for _dieValue, so that every variation of this object can be unique.
         }
 
-        public void Roll()
+        public void Roll() //Roll method is placed here, in order to generate a value between 1 and 6.
         {
-            _dieValue = rnd.Next(1, 7);
-            test.DieTest(_dieValue);
+            _dieValue = rnd.Next(1, 7); //Generates a value between 1 and 7 (works with the logic of any number that is: 1, 2, 3, 4, 5, 6 OR value >= 1 and value < 7)
+            test.DieTest(_dieValue); //Tests the value that is generated to make sure that the value is truly between 1 and 6.
         }
     }
 }
--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -9,16 +9,16 @@ namespace CMP1903_A1_2324
 {
     internal class Testing
     {
-        public void DieTest(int value)
+        public void DieTest(int value) // Creates a method for testing the Die class.
         {
-            Debug.Assert(value <= 6, "Die generated above the value of 6 (Max)");
-            Debug.Assert(value >= 1, "Die generated below the value of 1 (Min)");
+            Debug.Assert(value <= 6, "Die generated above the value of 6 (Max)"); //Using Asserts, the value generated is used to test if it exceeds logical maximums.
+            Debug.Assert(value >= 1, "Die generated below the value of 1 (Min)"); //Using Asserts, the value generated is used to test if it exceeds logical maximums.
         }
 
-        public void GameTest(int value)
+        public void GameTest(int value) // Creates a method for testing the Game class.
         {
-            Debug.Assert(value <= 18, "Sum above the value of 18 (Max)");
-            Debug.Assert(value >= 3, "Sum below the value of 3 (Min)");
+            Debug.Assert(value <= 18, "Sum above the value of 18 (Max)"); //Using Asserts, the sum is checked to make sure that it is below the logical maximum.
+            Debug.Assert(value >= 3, "Sum below the value of 3 (Min)"); //Using Asserts, the sum is checked to make sure that it is above the logical minimum.
         }
     }
 }
