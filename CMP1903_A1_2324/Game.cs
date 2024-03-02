96d047d0c49f7978871be5a6e4e83d290b20db49
Johnsa04 <anthony.johns2005@gmail.com>
2024-03-02 01:19:39 +0000

    first session.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -8,6 +8,12 @@ namespace CMP1903_A1_2324
 {
     internal class Game
     {
+        Die dice_1 = new Die();
+        Die dice_2 = new Die();
+        Die dice_3 = new Die();
+
+        int roll_total = dice_1.dice_roll_value + dice_2.dice_roll_value + dice_3.dice_roll_value
+        console.writeline("the sum of the dice rolls was ", roll_total )
         /*
          * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
          *
