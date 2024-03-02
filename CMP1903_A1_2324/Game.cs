49b0ca15c0b4584e22048179d5365da4f0a3d20b
Nedkins <nedkins09@gmail.com>
2024-03-02 19:33:02 +0000

    Fully coded and working

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -1,21 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.Diagnostics;
 
 namespace CMP1903_A1_2324
 {
-    internal class Game
+    public class Game
     {
-        /*
-         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
-         *
-         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
-         */
+        // gets random and creates 3 public die objects
+        private Random random;
+        public Die die1;
+        public Die die2;
+        public Die die3;
 
-        //Methods
+        public Game()
+        {
+            // assigns each die using the die class
+            random = new Random();
+            die1 = new Die(random);
+            die2 = new Die(random);
+            die3 = new Die(random);
+        }
+
+        public int Play()
+        {
+            // displays the values for each die and the total
+            int total = 0;
+            Console.WriteLine("Individual die rolls:");
+            total += die1.Roll();
+            Console.WriteLine("Die 1: " + die1.CurrentValue);
+            total += die2.Roll();
+            Console.WriteLine("Die 2: " + die2.CurrentValue);
+            total += die3.Roll();
+            Console.WriteLine("Die 3: " + die3.CurrentValue);
+            Console.WriteLine("Total of the three dice rolls: " + total);
+            return total;
+        }
 
     }
 }
--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CMP1903_A1_2324
 {
-    internal class Program
+    class Program
     {
+        // Main used to run program
         static void Main(string[] args)
         {
-            /*
-             * Create a Game object and call its methods.
-             * Create a Testing object to verify the output and operation of the other classes.
-             */
+            Testing.Test();
+            Console.ReadKey();
         }
     }
 }
