7f469ae6601edb2504a027cc65dbe1c6b34f8aa8
magoed12 <107357062+magoed12@users.noreply.github.com>
2024-03-04 20:25:56 +0000

    V 1.0
    First complete version

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -22,7 +22,7 @@ namespace CMP1903_A1_2324
             Die die3 = new Die();
             int value3 = die3.Roll();
             Thread.Sleep(1);
-            return (die1: die1.Dieval, die2: die2.Dieval, die3: die3.Dieval, sum: die1.Dieval + die2.Dieval + die3.Dieval);
+            return (die1: die1.Dieval , die2: die2.Dieval, die3: die3.Dieval, sum: die1.Dieval + die2.Dieval + die3.Dieval);
         }
     }
 }
--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -14,10 +14,10 @@ namespace CMP1903_A1_2324
             Game game = new Game();
             var values = game.RunGame();
             Console.WriteLine(values.Item4);
-            Debug.Assert(values.Item4 > 18 || values.Item4 < 3, "The sum is incorrect");
-            Debug.Assert(values.Item1 > 6 || values.Item1 < 1, "A die roll is invalid");
-            Debug.Assert(values.Item2 > 6 || values.Item2 < 1, "A die roll is invalid");
-            Debug.Assert(values.Item3 > 6 || values.Item3 < 1, "A die roll is invalid");
+            Debug.Assert(values.Item4 < 19 && values.Item4 > 2, "The sum is incorrect");
+            Debug.Assert(values.Item1 < 7 && values.Item1 > 0, "A die roll is invalid");
+            Debug.Assert(values.Item2 < 7 && values.Item2 > 0, "A die roll is invalid");
+            Debug.Assert(values.Item3 < 7 && values.Item3 > 0, "A die roll is invalid");
         }
     }
 }
