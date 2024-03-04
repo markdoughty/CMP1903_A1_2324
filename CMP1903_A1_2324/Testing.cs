e60212c18c9a5e8ecbb98460255d3b8a00e92911
yazz3660 <yazz.alderson@gmail.com>
2024-03-04 23:47:35 +0000

    Testing Class

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Testing
+    public class Testing
     {
-        /*
-         * This class should test the Game and the Die class.
-         * Create a Game object, call the methods and compare their output to expected output.
-         * Create a Die object and call its method.
-         * Use debug.assert() to make the comparisons and tests.
-         */
+        public void Test()
+        {
+            //Creating a Game object
+            Game GameTest = new Game();
 
-        //Method
+            GameTest.DieCreator();
+
+            Die DieTest = new Die();
+            DieTest.DieValue = DieTest.Roll();
+            Console.WriteLine("\nDie Test: " + DieTest.DieValue);
+            Console.WriteLine("Press any key to exit...");
+
+            Debug.Assert(DieTest.DieValue > 0 && DieTest.DieValue < 7);
+            Debug.Assert(GameTest.result == GameTest.Die1.DieValue + GameTest.Die2.DieValue + GameTest.Die3.DieValue);
+        }
     }
 }
