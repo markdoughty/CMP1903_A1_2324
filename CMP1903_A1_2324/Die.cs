eddf68e43bd95f6fcd36b84771109be565d4a6ba
MatthewSole101 <91541357+MatthewSole101@users.noreply.github.com>
2024-03-04 13:36:19 +0000

    Add files via upload

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -26,40 +26,10 @@ namespace CMP1903_A1_2324
         //Created the Roll method to randomly roll a die three times
         public  int Roll()
         {
+
+            int randNum = randNum.Next(1, 7);
             
-            List<int> RollList = new List<int>();
-            Random rand = new Random();
-
-
-            //Randomly rolling 3 times and saves into a list
-            for (int i = 1; i <= 3; i++)
-                RollList.Add(rand.Next(1, 7));
-
-            if (count1 == 0 )
-            {
-                Rolls = RollList[0];
-                count1 = count1 + 1;
-                return RollList[0];
-            }
-            if (count1 == 1)
-            {
-                count1 = count1 + 1;
-                return RollList[1];
-            }
-            if (count1 == 2)
-            {
-                count1 = count1 + 1;
-                return RollList[2];
-            }
-
-
-
-            Console.WriteLine();
-            
-
-
-            //the rolls get returned 
-            return 0;
+            return randNum;
             
         }
 
