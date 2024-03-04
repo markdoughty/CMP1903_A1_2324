a42707c9c66cc7a540bcb09a6394e0a6420cc85e
Nedkins <nedkins09@gmail.com>
2024-03-04 20:25:41 +0000

    Added Expert Comments
    Comments

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -5,17 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 
-
-namespace CMP1903_A1_2324
+namespace DieGame_Assessment_1
 {
+    // Testing class
     public class Testing
     {
+        // Creates the Test method (this is called from the program class to start the game)
         public static void Test()
         {
+            // Creates a new instance of the game class
             Game game = new Game();
-            int total = game.Play();
+            int total = game.Play(); // Calls the Play method from the game class
 
-            // Debug asserting to find individual die rolls
+            // Debug asserting to find individual die rolls and make sure each die is within range (if not then displays a message to the user)
             Debug.Assert(game.die1.CurrentValue >= 1 && game.die1.CurrentValue <= 6, "Die 1 is out of range");
             Debug.Assert(game.die2.CurrentValue >= 1 && game.die2.CurrentValue <= 6, "Die 2 is out of range");
             Debug.Assert(game.die3.CurrentValue >= 1 && game.die3.CurrentValue <= 6, "Die 3 is out of range");
@@ -24,6 +26,7 @@ namespace CMP1903_A1_2324
             int expectedTotal = game.die1.CurrentValue + game.die2.CurrentValue + game.die3.CurrentValue;
             Debug.Assert(total == expectedTotal, $"Unexpected total: {total}");
 
+            // Output message if all the tests are passed
             Console.WriteLine("Tests passed successfully.");
         }
     }
