d2692a29839376b0488c2b816bad199bfa90e23d
yazz3660 <yazz.alderson@gmail.com>
2024-03-04 23:46:14 +0000

    Program Class (Main Function)

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -10,10 +10,12 @@ namespace CMP1903_A1_2324
     {
         static void Main(string[] args)
         {
-            /*
-             * Create a Game object and call its methods.
-             * Create a Testing object to verify the output and operation of the other classes.
-             */
+            Game Game1 = new Game();
+
+            Game1.DieCreator();
+
+            Testing Test1 = new Testing();
+
         }
     }
 }
