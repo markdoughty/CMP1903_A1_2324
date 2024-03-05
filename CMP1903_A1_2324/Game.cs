04304c5cb336fac0bdd12e4a9375859ef3a62b56
lewis maney <108680931+madlew22255@users.noreply.github.com>
2024-03-05 21:14:20 +0000

    commit 1
    program works as expected (need to complete the testing class)

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -14,8 +14,29 @@ namespace CMP1903_A1_2324
          * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
          * rolls could be continous, and the totals and other statistics could be summarised for example.
          */
+        int DieTotal; 
 
         //Methods
 
+
+        public int DieSum() 
+        {
+            Die die = new Die();
+            int DieRoll = die.Roll();
+            Console.WriteLine("Roll 1: " + DieRoll);
+
+            Die die1 = new Die();
+            int DieRoll1 = die.Roll();
+            Console.WriteLine("Roll 2: " + DieRoll1);
+            
+            Die die2 = new Die();
+            int DieRoll2 = die.Roll();
+            Console.WriteLine("Roll 3: " + DieRoll2);
+
+            int DieTotal = DieRoll + DieRoll1 + DieRoll2;
+            Console.WriteLine("Die total: " + DieTotal);
+
+            return DieTotal;
+        }
     }
 }
