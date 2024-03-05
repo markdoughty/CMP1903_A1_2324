36c534aa4276090087e24b81e1162cf95afee8fc
James Busby <146133909+JamesB05@users.noreply.github.com>
2024-03-05 15:22:05 +0000

    Commit changes

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -10,10 +10,19 @@ namespace CMP1903_A1_2324
     {
         static void Main(string[] args)
         {
+
+
+            Game game = new Game();
+            Console.WriteLine($"Total Sum is: {game.Sum}");
+
+            Testing.RunTests();
             /*
              * Create a Game object and call its methods.
              * Create a Testing object to verify the output and operation of the other classes.
              */
         }
     }
+    
+
+
 }
