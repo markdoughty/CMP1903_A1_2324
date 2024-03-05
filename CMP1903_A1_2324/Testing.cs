26cdf1bc5c4892ed6f403cf53243e98f2e42f5e1
MatthewPage1 <158061923+MatthewPage1@users.noreply.github.com>
2024-03-05 22:20:26 +0000

    Update Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,24 @@ namespace CMP1903_A1_2324
          */
 
         //Method
+        public static void TestGame()
+        {
+            Game testGame = new Game();
+            int TotalValues = testGame.Total;
+
+            Debug.Assert(TotalValues >= 3 && TotalValues <= 18, $"Sum of die values ({TotalValues}) not between 3 and 18"); //Check if the sum of the values is between 3 and 18
+            
+        }
+
+        public static void TestDie()
+        {
+            Die testDie = new Die();
+
+            int DieValue = testDie.RollDie();     //roll die and see if value is between 1 and 6
+            Debug.Assert(DieValue >= 1 && DieValue <= 6, $"Die roll ({DieValue}) not between 1 and 6");
+
+
+        }
+
     }
 }
