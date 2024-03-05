4d0213fbb672d8708392e088a306b4e693b331c8
KapaBaws <158062142+KapaBaws@users.noreply.github.com>
2024-03-05 22:20:38 +0000

    Final Commit.

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -18,15 +18,5 @@ namespace CMP1903_A1_2324
             int total = game.diceGame();
             Debug.Assert(total >= 3 && total <= 18, "The result do not meet the upper and lower bounds.");
         }
-
-
-        /*
-         * This class should test the Game and the Die class.
-         * Create a Game object, call the methods and compare their output to expected output.
-         * Create a Die object and call its method.
-         * Use debug.assert() to make the comparisons and tests.
-         */
-
-        //Method
     }
 }
