72ff23ff6dbb3a09f4d130a16407550904d40283
EJohnston2 <162377589+EJohnston2@users.noreply.github.com>
2024-03-05 22:16:58 +0000

    Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -12,12 +12,22 @@ namespace CMP1903_A1_2324
          * The Die class should contain one property to hold the current die value,
          * and one method that rolls the die, returns and integer and takes no parameters.
          */
+        int _rollValue;
+        int _total;
 
-        //Property
+        public int Roll(Random _rand)
+        { 
+            _rollValue = int _rand.Next(1, 7, 1);
+            return _rollValue;
+            _total = int _rollValue + int _total;
+            return _total
+        }
+        //simulates a dice roll
+        //produces a random value between 1 and 6
+        //returns the value of the dice
+        //dice value is added the round total
+        //returns the value of the total counter
 
-
-        //Method
-
-
+        
     }
 }
