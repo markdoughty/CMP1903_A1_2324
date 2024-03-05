96b03ee346fdb8a8acacfe725ce8473f1625bf44
black-eye-inc <27047602@students.lincoln.ac.uk>
2024-03-05 14:24:17 +0000

    add darm SketchyDevelopment file!
    2nd attempt

---
+++ SketchyDevelopment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903_A1_2324
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!!!");
+            Console.ReadKey();
+            /*
+             * Create a Game object and call its methods.
+             * Create a Testing object to verify the output and operation of the other classes.
+             */
+        }
+    }
+}
