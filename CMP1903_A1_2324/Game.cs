ea361e207007c735db7cb17aea628b5f246b8ed0
Richstar1 <158062123+Richstar1@users.noreply.github.com>
2024-03-06 11:41:01 +0000

    Update Game.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -6,39 +6,42 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    static void Main()
+    class GameSimulation
     {
-        Die die1 = new Die();
-        Die die2 = new Die();
-        Die die3 = new Die();
-
-        bool continueRolling = true;
-
-        while (continueRolling)
+        static void Main()
         {
-            int numberOfRolls = 3;
+            Die die1 = new Die();
+            Die die2 = new Die();
+            Die die3 = new Die();
 
-            Tester.TestDieValues(die1, numberOfRolls);
-            Tester.TestDieValues(die2, numberOfRolls);
-            Tester.TestDieValues(die3, numberOfRolls);
+            bool continueRolling = true;
 
-            for (int i = 0; i < numberOfRolls; i++)
+            while (continueRolling)
             {
-                int rollResult1 = die1.RollDie();
-                int rollResult2 = die2.RollDie();
-                int rollResult3 = die3.RollDie();
+                int numberOfRolls = 3;
 
-                int totalSum = rollResult1 + rollResult2 + rollResult3;
+                Tester.TestDieValues(die1, numberOfRolls);
+                Tester.TestDieValues(die2, numberOfRolls);
+                Tester.TestDieValues(die3, numberOfRolls);
 
-                Console.WriteLine($"Roll {i + 1}: Die1: {rollResult1}, Die2: {rollResult2}, Die3: {rollResult3}, Total: {totalSum}");
-            }
+                for (int i = 0; i < numberOfRolls; i++)
+                {
+                    int rollResult1 = die1.RollDie();
+                    int rollResult2 = die2.RollDie();
+                    int rollResult3 = die3.RollDie();
 
-            Console.WriteLine("Type 'continue' to roll again or any other key to exit:");
-            string userInput = Console.ReadLine();
+                    int totalSum = rollResult1 + rollResult2 + rollResult3;
 
-            if (!userInput.ToLower().Equals("continue"))
-            {
-                continueRolling = false;
+                    Console.WriteLine($"Roll {i + 1}: Die1: {rollResult1}, Die2: {rollResult2}, Die3: {rollResult3}, Total: {totalSum}");
+                }
+
+                Console.WriteLine("Type 'continue' to roll again or any other key to exit:");
+                string userInput = Console.ReadLine();
+
+                if (!userInput.ToLower().Equals("continue"))
+                {
+                    continueRolling = false;
+                }
             }
         }
     }
