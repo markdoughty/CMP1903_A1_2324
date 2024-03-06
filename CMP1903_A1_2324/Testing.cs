dbfb3dd11cc964707f87bc458cc91fd74e4c9aa4
JakeH112233 <162054718+JakeH112233@users.noreply.github.com>
2024-03-06 13:27:36 +0000

    Update Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -6,15 +6,33 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Testing
+    static void Main(string[] args)
     {
-        /*
-         * This class should test the Game and the Die class.
-         * Create a Game object, call the methods and compare their output to expected output.
-         * Create a Die object and call its method.
-         * Use debug.assert() to make the comparisons and tests.
-         */
+        Console.WriteLine("The dice roll game"); //displays to the user the game has started
 
-        //Method
+        Console.Write("Enter the number of dice to roll: "); //Allows the user to enter number of dice to be rolled
+        int num_Dice = int.Parse(Console.ReadLine()); //Converts string from the user into an integer value
+
+        Game game = new Game(num_Dice); //Displays the new game based on the num of dice declared
+
+        Console.WriteLine("The dice roll game"); //repeats the game if the user rerolls
+
+        bool reroll = true; //Initially sets the reroll as true 
+
+         while (reroll)
+        {
+            Console.WriteLine("\nDice set"); 
+            game.Program();
+
+            string rerollInput; //allows the user to input the reroll choice
+
+            do
+            {
+                Console.Write("Do you want to reroll? (Y/N): ");
+                rerollInput = Console.ReadLine().ToLower(); 
+            } while (rerollInput != "Y" && rerollInput != "N"); //if user inputs something other than "Y" or "N" it loops back
+
+            reroll = rerollInput == "Y"; //Rolls if Y
+        }
     }
 }
