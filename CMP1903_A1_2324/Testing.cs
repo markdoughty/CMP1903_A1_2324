86f131be783e22e9688195d8382ace76ce399d9b
Davo <daviddallakyan17@gmail.com>
2024-03-06 09:34:27 +0000

    Made changes to project

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -1,20 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace CMP1903_A1_2324
 {
     internal class Testing
     {
-        /*
-         * This class should test the Game and the Die class.
-         * Create a Game object, call the methods and compare their output to expected output.
-         * Create a Die object and call its method.
-         * Use debug.assert() to make the comparisons and tests.
-         */
+        public static void RunTests()
+        {
+            TestDie();
+            TestGame();
+        }
 
-        //Method
+        private static void TestDie()
+        {
+            Die die = new Die();
+            die.Roll();
+            Debug.Assert(die.CurrentValue >= 1 && die.CurrentValue <= 6, "Die roll result out of expected range");
+        }
+
+        private static void TestGame()
+        {
+            Game game = new Game();
+            int total = game.RollDice();
+            Debug.Assert(total >= 3 && total <= 18, "Game total out of expected range");
+        }
     }
 }
