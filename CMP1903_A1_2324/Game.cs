fb0b0defad4988700b602deb484396f04fcf5101
JakeColes04 <158061889+JakeColes04@users.noreply.github.com>
2024-03-06 22:57:46 +0000

    Added rolling dice to game.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -17,5 +17,23 @@ namespace CMP1903_A1_2324
 
         //Methods
 
+        private Die die1 = new Die();
+        private Die die2 = new Die();
+        private Die die3 = new Die();
+
+        public void rolling()
+        {
+            int roll1 = die1.Roll();
+            int roll2 = die2.Roll();
+            int roll3 = die3.Roll();
+            System.Console.WriteLine("Roll 1 is" + roll1);
+            System.Console.WriteLine("Roll 2 is" + roll2);
+            System.Console.WriteLine("Roll 3 is" + roll3);
+            System.Console.WriteLine("Your total is" + (roll1 + roll2 + roll3) );
+
+
+        }
+
+        
     }
 }
