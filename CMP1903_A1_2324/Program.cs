6f82824fbca687d056a3609e6eb39b29fa7726fc
yipped <162243613+yipped@users.noreply.github.com>
2024-03-06 19:43:18 +0000

    made classes die, game and program
    need to finish testing class

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -8,12 +8,24 @@ namespace CMP1903_A1_2324
 {
     internal class Program
     {
+        //Random rand = new Random((int)DateTime.Now.Ticks);
+
         static void Main(string[] args)
         {
             /*
              * Create a Game object and call its methods.
              * Create a Testing object to verify the output and operation of the other classes.
              */
+            while(true)
+            {
+                Game game = new Game();
+                game.PlayGame();
+            }
+            /*Game game = new Game();
+            game.PlayGame();
+
+            Testing testing = new Testing();*/
+
         }
     }
 }
