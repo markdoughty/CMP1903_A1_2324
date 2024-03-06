ecb5c2be7ec03707eb6b5157cdadefba691a6d17
JakeH112233 <162054718+JakeH112233@users.noreply.github.com>
2024-03-06 14:50:03 +0000

    Update Game.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-public class Game
-{
-    private Die[] dice;
-    private int num_Dice;
+    public class Game{
+        
+        private Die[] dice;
+        private int num_Dice;
 
-    public Game(int num_Dice)
-    {
-        this.num_Dice = num_Dice;
-        dice = new Die[num_Dice]; //Sets dice equal to total num of die set
-        for (int x = 0; x < num_Dice; x++) //loops rolling dice for the specified number of times
-        {
-            dice[x] = new Die(); //Creates the dice objects
+        public Game(int num_Dice){
+            
+            this.num_Dice = num_Dice;
+            dice = new Die[num_Dice]; //Sets dice equal to total num of die set
+            for (int x = 0; x < num_Dice; x++) //loops rolling dice for the specified number of times{
+            
+                dice[x] = new Die(); //Creates the dice objects
         }
     }
 }
