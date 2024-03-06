778e31de56b300a1f25a43399b9009634f43551a
yipped <162243613+yipped@users.noreply.github.com>
2024-03-06 21:33:29 +0000

    added testing class
    added testing class and changed protection level of variables to allow for proper testing

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -15,23 +15,32 @@ namespace CMP1903_A1_2324
          * rolls could be continous, and the totals and other statistics could be summarised for example.
          */
 
+        //needs to be accessed by testing class
+        public Die one, two, three;
+        public int sum;
+        public int roll1, roll2, roll3;
+
         //Methods
         //handles creating die objects, calculating sum and displaying to console
         public void PlayGame()
         {
             //Creates three die objects
-            Die one = new Die();
-            Die two = new Die();
-            Die three = new Die();
-
-            //old way of recording sum
-            //int sum = RollDie(one, 1) + RollDie(two, 2) + RollDie(three, 3);
+            one = new Die();
+            two = new Die();
+            three = new Die();
 
             //Tells user to press enter, done to make numbers random as time passes changing the random seed
             Console.WriteLine("Press enter to roll each dice!");
 
-            //Calculates the sum by rolling each die individually, adding return values, showing resulting sum to user
-            Console.WriteLine("The sum is: " + (RollDie(one, 1) + RollDie(two, 2) + RollDie(three, 3)) + "!!! END\nPress enter to continue...");
+            //Rolls the three dice and records output for testing class
+            roll1 = RollDie(one, 1);
+            roll2 = RollDie(two, 2);
+            roll3 = RollDie(three, 3);
+
+            sum = roll1 + roll2 + roll3;
+
+            //Shows results to user
+            Console.WriteLine("The sum is: " + sum + "!!! END\nPress enter to continue...");
             Console.ReadLine();
         }
 
