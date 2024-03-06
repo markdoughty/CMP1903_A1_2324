3bf4742595bfad1511fa22ee6fb9e19a0ee04755
ClaudioIldefonso <158074343+ClaudioIldefonso@users.noreply.github.com>
2024-03-06 17:16:08 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace CMP1903_A1_2324
 {
@@ -12,11 +13,36 @@ namespace CMP1903_A1_2324
          * The Die class should contain one property to hold the current die value,
          * and one method that rolls the die, returns and integer and takes no parameters.
          */
+        private int _DieValue;
+        private Random random;
 
         //Property
 
+        public int DieValue
+        {
+            get { return _DieValue; }
+            set { _DieValue = value; }
+
+        }
+
 
         //Method
+        public int Roll()
+        {
+            Thread.Sleep(450);
+            _DieValue = random.Next(1, 7);
+            return _DieValue;
+            //return random.Next(1, 7);
+        }
+        public Die()
+        {
+            random = new Random();
+
+            //Roll();
+
+        }
+
+
 
 
     }
