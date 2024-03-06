04bd8ec57278829b2286a7f577c05b8732f697ff
BenPurch <162333527+BenPurch@users.noreply.github.com>
2024-03-06 12:22:10 +0000

    Add files via upload

---
+++ CMP1903_A1_2324/CMP1903_A1_2324/Die.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903_A1_2324
+{
+    internal class Die
+    {
+        /*
+         * The Die class should contain one property to hold the current die value,
+         * and one method that rolls the die, returns and integer and takes no parameters.
+         */
+
+        //Property
+        public int RollNumber {  get; set; }
+
+        //Method
+        public int Roll()
+        {
+            //Generating the random number 1-6 and returning that number
+            Random random = new Random();
+            RollNumber = random.Next(1, 7);
+            return RollNumber;
+        }
+
+    }
+}
---
+++ CMP1903_A1_2324/CMP1903_A1_2324/Testing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903_A1_2324
+{
+    internal class Testing
+    {
+        /*
+         * This class should test the Game and the Die class.
+         * Create a Game object, call the methods and compare their output to expected output.
+         * Create a Die object and call its method.
+         * Use debug.assert() to make the comparisons and tests.
+         */
+
+        //Method
+        public void TestGame()
+        {
+            //Testing the game class
+            Game game = new Game();
+            int total = game.RollCombo();
+            Debug.Assert(total >= 3 && total <= 18, "Total roll should be within 3 and 18");
+        }
+        public void TestDie()
+        {
+            //Testing the die class
+            Die die = new Die();
+            int roll = die.Roll();
+            Debug.Assert(roll >= 1 && roll <= 6, "Total roll should be within 3 and 18");
+        }
+    }
+}
