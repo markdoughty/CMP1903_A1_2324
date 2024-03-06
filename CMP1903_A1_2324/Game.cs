1e42eda3030093155778ab076de831f2ef3b1bbc
Richstar1 <158062123+Richstar1@users.noreply.github.com>
2024-03-06 11:38:31 +0000

    Update Game.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -6,16 +6,40 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Game
+    static void Main()
     {
-        /*
-         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
-         *
-         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
-         */
+        Die die1 = new Die();
+        Die die2 = new Die();
+        Die die3 = new Die();
 
-        //Methods
+        bool continueRolling = true;
 
+        while (continueRolling)
+        {
+            int numberOfRolls = 3;
+
+            Tester.TestDieValues(die1, numberOfRolls);
+            Tester.TestDieValues(die2, numberOfRolls);
+            Tester.TestDieValues(die3, numberOfRolls);
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int rollResult1 = die1.RollDie();
+                int rollResult2 = die2.RollDie();
+                int rollResult3 = die3.RollDie();
+
+                int totalSum = rollResult1 + rollResult2 + rollResult3;
+
+                Console.WriteLine($"Roll {i + 1}: Die1: {rollResult1}, Die2: {rollResult2}, Die3: {rollResult3}, Total: {totalSum}");
+            }
+
+            Console.WriteLine("Type 'continue' to roll again or any other key to exit:");
+            string userInput = Console.ReadLine();
+
+            if (!userInput.ToLower().Equals("continue"))
+            {
+                continueRolling = false;
+            }
+        }
     }
 }
