263f7f37483d7cfa5601124d1d7fb3d6b3c34987
JakeH112233 <162054718+JakeH112233@users.noreply.github.com>
2024-03-06 13:13:56 +0000

    Update Game.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -9,13 +9,15 @@ namespace CMP1903_A1_2324
 public class Game
 {
     private Die[] dice;
+    private int num_Dice;
 
-    public Game()
+    public Game(int num_Dice)
     {
-        dice = new Die[3]; //Allows the amount of dice that are added to roll
-        for (int x = 0; x < 3; x++) //For loop to run the dice rolls until the specified number has been met which is 3 dice
+        this.num_Dice = num_Dice;
+        dice = new Die[num_Dice]; //Sets dice equal to total num of die set
+        for (int x = 0; x < num_Dice; x++) //loops rolling dice for the specified number of times
         {
-            dice[x] = new Die(); //Creates the Dice object based on the number of times the loop is ran
+            dice[x] = new Die(); //Creates the dice objects
         }
     }
 }
