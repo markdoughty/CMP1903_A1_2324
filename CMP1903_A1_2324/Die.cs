f298bb0d152f45de159be28365cce0dc2e6a72cb
JakeH112233 <162054718+JakeH112233@users.noreply.github.com>
2024-03-06 14:48:53 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -4,13 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Die
-{
+public class Die{
     private static Random random = new Random(); //Sets random as a new random in order to roll a number from 1-6 randomly
     public int Value { get; private set; } //Sets a new integer value for the dice to store the random number
 
-    public int Roll()
-    {
+    public int Roll(){
         Value = random.Next(1, 7); //Generates random number between 1-6 with 6 included
         return Value; //Returns the generated number to the specified die
     }
