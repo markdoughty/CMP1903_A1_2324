96d8362850956d10b3e9d864680a94e8d3f497e9
olanrewaju139 <160535664+olanrewaju139@users.noreply.github.com>
2024-03-06 13:30:49 -0500

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -11,7 +11,7 @@ namespace CMP1903_A1_2324
         
 
         //Method
-        public int HoldValue { get; private set; } //to store the value gotten from rolling the dice
+        public int holdValue { get; private set; } //to store the value gotten from rolling the dice
         // <black-eye-inc> Code Review:
         // I'd recommend using _camelCase (calling it "_holdValue") for private parameters (as instructed by "C# style guidelines")
         // one of the Object-Oriented Principles is encapsulation (making all parameters (within reason) private).
@@ -20,9 +20,9 @@ namespace CMP1903_A1_2324
         {
             Random rand = new Random();
             //create a random number between 1 and 6 then stores it in holdValue
-            HoldValue = rand.Next(1, 7); 
+            holdValue = rand.Next(1, 7); 
 
-            return HoldValue;
+            return holdValue;
         }
 
 
