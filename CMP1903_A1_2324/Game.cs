cb14a6d83d65d1e161c2fefb6846be4a209110d1
ClaudioIldefonso <158074343+ClaudioIldefonso@users.noreply.github.com>
2024-03-06 19:13:26 +0000

    Update Game.cs
    finalfinal

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -8,21 +8,13 @@ namespace CMP1903_A1_2324
 {
     internal class Game
     {
+        // allows for arrays []
         private Die[] dice;
-
-
-
-        /*
-         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
-         *
-         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
-         */
-
-        //Methods
+       
+        // forms a 3 dice array using the Die class
         public Game()
         {
-
+            // for statment allows the Dice total to only add up 3 die
             dice = new Die[3];
             for (int i = 0; i < 3; i++)
             {
@@ -30,21 +22,15 @@ namespace CMP1903_A1_2324
             }
 
         }
-
+        /* this function adds up the total of the 3 die */
         public int DiceTotal()
         {
             int Total = 0;
             foreach (Die die in dice)
             {
-                //die.Roll();
-                //Total += die.DieTotal;
                 Total += die.Roll();
             }
-
             return Total;
-
-
         }
-
     }
 }
