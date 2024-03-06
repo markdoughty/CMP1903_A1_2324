dab4f71a82138f190ed90085443af9417d8e8d04
EJohnston2 <162377589+EJohnston2@users.noreply.github.com>
2024-03-06 20:35:46 +0000

    Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -15,19 +15,19 @@ namespace CMP1903_A1_2324
          * Use debug.assert() to make the comparisons and tests.
          */
 
-        public Game _testGame = new Game();
-        public Die _testDie = new Die();
+        public Game TestGame = new Game();
+        public Die TestDie = new Die();
 
         //creates a new game and die class
 
-        Debug.Assert(_roll1 >= 1 and _roll1 <= 6);
-        Debug.Assert(_roll2 >= 1 and _roll2 <=6);
-        Debug.Assert(_roll3 >= 1 and _roll3 <=6);
+        Debug.Assert(Roll1 >= 1 && Roll1 <= 6);
+        Debug.Assert(Roll2 >= 1 && Roll2 <=6);
+        Debug.Assert(Roll3 >= 1 && Roll3 <=6);
         Console.Writeline($"Boundary error: Invalid value for the die")
 
         //tests boundaries of the roll values to ensure all values are between 1 and 6
 
-        Debug.Assert((_roll1 + _roll2 + _roll3) == Game._dieTotal()); 
+        Debug.Assert((Roll1 + Roll2 + Roll3) == Game.DieTotal()); 
         Console.Writeline($"Unexpected error: Invalid error for die")
 
         //tests that the sum of the die is as expected
