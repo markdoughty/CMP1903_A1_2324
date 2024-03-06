7ee9646e0c0319637a4663b441a2834a84b03e8c
JakeH112233 <162054718+JakeH112233@users.noreply.github.com>
2024-03-06 13:03:22 +0000

    Update Program.cs

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -6,14 +6,20 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Program
+    public void Program()
     {
-        static void Main(string[] args)
+        int total = 0; //Sets the total of the combined dice to 0 so it has a value
+
+        Console.WriteLine("The following amount of Dice are being rolled..."); //Allows the user to see that the following dice are being rolled
+
+        for (int x = 0; x < 3; x++) //For loop to run the dice rolls until the specified number has been met which is 3 dice
         {
-            /*
-             * Create a Game object and call its methods.
-             * Create a Testing object to verify the output and operation of the other classes.
-             */
+            int roll_value = dice[x].Roll(); //Sets the roll_value equal to the result of the roll function in the Die class
+            Console.WriteLine($"Die {x + 1} Value: {roll_value}"); //Displays which die rolled what value for the user
+            total += roll_value; //Totals up the value of all the dice combined and gives a accumulative sum
         }
+
+        Console.WriteLine($"Total sum of all dice rolls combined: {total}"); //Displays the total to the user so they can see the score
+        Debug.Assert(total >= 3 && total <= 18, "Total is greater or less than the possible combination of totals."); //Checks to make sure that the value cannot be creater than 18 or less that 3 as its not in the possible combination of die roll totals
     }
 }
