999caae1ad6ba9749656ce97f8bbcc950ae39044
Keven CM Cunha <119662366+kauaw19@users.noreply.github.com>
2024-03-07 13:00:22 +0000

    Update Game.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -6,28 +6,26 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Game
+    public class Game
     {
-        public class Game
+        private readonly Die number1;
+        private readonly Die number2;
+        private readonly Die number3;
+        public Game() 
+        {   
+            number1 = new Die();
+            number2 = new Die();
+            number3 = new Die();
+        }
+        public int TotalValueOfDices() 
         {
-            private readonly Die number1;
-            private readonly Die number2;
-            private readonly Die number3;
-            public Game() 
-            {   
-                number1 = new Die();
-                number2 = new Die();
-                number3 = new Die();
-            }
-            public int TotalValueOfDices() 
-            {
-                return number1.Roll() + number2.Roll() + number3.Roll();
-            } 
-            public void PrintTotalValueOfDices()
-            {
-                int sum = TotalValueOfDices();
-                Console.WriteLine("The sum of TotalValueOfDices is: " + sum);
-            }
+            return number1.Roll() + number2.Roll() + number3.Roll();
+        } 
+        public void PrintTotalValueOfDices()
+        {
+            int sum = TotalValueOfDices();
+            Console.WriteLine("The sum of TotalValueOfDices is: " + sum);
         }
     }
+    
 }
