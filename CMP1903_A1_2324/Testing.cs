adb56747829d4016e473894c37345f0bae7daed6
Keven CM Cunha <119662366+kauaw19@users.noreply.github.com>
2024-03-07 13:01:27 +0000

    Update Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -6,37 +6,36 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Testing
+   
+    public class Testing
     {
-       public class Testing
+        public void TestDiceRolls()
         {
-            public void TestDiceRolls()
+            Die die = new Die();
+            int roll = die.Roll();
+            if (roll < 1 || roll > 6)
             {
-                Die die = new Die();
-                int roll = die.Roll();
-                if (roll < 1 || roll > 6)
-                {
-                    Console.WriteLine("Die roll is not between 1 and 6.");
-                }
-                else
-                {
-                    Console.WriteLine("Die roll is between 1 and 6.");
-                }   
+                Console.WriteLine("Die roll is not between 1 and 6.");
             }
-            public void TestGameSum()
+            else
             {
-                Game game = new Game();
-                int sum = game.TotalValueOfDices();
+                Console.WriteLine("Die roll is between 1 and 6.");
+            }   
+        }
+        public void TestGameSum()
+        {
+            Game game = new Game();
+            int sum = game.TotalValueOfDices();
                 
-                if (sum < 3 || sum > 18)
-                {
-                    Console.WriteLine("The sum of the three values is not as expected.");
-                }
-                else
-                {
-                    Console.WriteLine("The sum of the three values is as expected.");
-                }
+            if (sum < 3 || sum > 18)
+            {
+                Console.WriteLine("The sum of the three values is not as expected.");
+            }
+            else
+            {
+                Console.WriteLine("The sum of the three values is as expected.");
             }
         }
     }
+    
 }
