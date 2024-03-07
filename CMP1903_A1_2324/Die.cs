7772ea2e2e4d57b387d063d52e4f00b1455baf26
Madjam05 <162447804+Madjam05@users.noreply.github.com>
2024-03-07 00:12:14 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -12,12 +12,18 @@ namespace CMP1903_A1_2324
          * The Die class should contain one property to hold the current die value,
          * and one method that rolls the die, returns and integer and takes no parameters.
          */
+        
 
         //Property
-
+        public int value //Created a place to store dice values.
 
         //Method
+        public int dice_roll()
+        {
+            value = rnd.next(1,7)
 
+            return value
+        }
 
     }
 }
