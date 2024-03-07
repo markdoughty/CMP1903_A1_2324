0c9720e49a64ba5df46f610a2565ba88e1443f34
Keven CM Cunha <119662366+kauaw19@users.noreply.github.com>
2024-03-07 13:10:34 +0000

    Update Game.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -8,23 +8,23 @@ namespace CMP1903_A1_2324
 {
     public class Game
     {
-        private readonly Die number1;
-        private readonly Die number2;
-        private readonly Die number3;
+        private readonly Die number1; // creates a object
+        private readonly Die number2; // creates a object
+        private readonly Die number3; // creates a object
         public Game() 
         {   
-            number1 = new Die();
-            number2 = new Die();
-            number3 = new Die();
+            number1 = new Die(); //use class die to create the random number
+            number2 = new Die();//use class die to create the random number
+            number3 = new Die();//use class die to create the random number
         }
         public int TotalValueOfDices() 
         {
-            return number1.Roll() + number2.Roll() + number3.Roll();
+            return number1.Roll() + number2.Roll() + number3.Roll(); // rools and sum all the numbers
         } 
         public void PrintTotalValueOfDices()
         {
             int sum = TotalValueOfDices();
-            Console.WriteLine("The sum of TotalValueOfDices is: " + sum);
+            Console.WriteLine("The sum of TotalValueOfDices is: " + sum);  // output the sum of the number to the user
         }
     }
     
