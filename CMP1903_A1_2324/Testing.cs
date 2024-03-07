4cd68f52be849319e9625f86cd5ae381ada9de08
KPKP2016 <145162925+KPKP2016@users.noreply.github.com>
2024-03-07 11:44:07 +0000

    Revert "Code review for OOP DiceGame."

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,29 +8,13 @@ namespace CMP1903_A1_2324
 {
     internal class Testing
     {
-        // runs both methods
-        public Testing() {
-            TestDiceRollValue();
-            TestDiceRollSum();
-        }
-        public void TestDiceRollValue()
-        {
-            // Creates new TestVal object
-            Die test = new Die();
-            int TestVal = test.Roll();
+        /*
+         * This class should test the Game and the Die class.
+         * Create a Game object, call the methods and compare their output to expected output.
+         * Create a Die object and call its method.
+         * Use debug.assert() to make the comparisons and tests.
+         */
 
-            // Tests if die value is inside bounds
-            Debug.Assert(TestVal > 0 && TestVal < 7, "Dice value is out of bounds."); ;
-        }
-                
-        public void TestDiceRollSum()
-        {
-            // Creates new TestSum object
-            Game testPlay = new Game();
-            int TestSum = testPlay.DiceGame();
-
-            // Tests if sum is inside bounds
-            Debug.Assert(TestSum > 2 && TestSum < 19, "Total sum of the rolled dice, is out of bounds");
-        }
+        //Method
     }
 }
