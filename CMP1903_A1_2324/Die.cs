baad88e3b377ec49616ac7568e3ddd0958a3f588
Keven CM Cunha <119662366+kauaw19@users.noreply.github.com>
2024-03-07 13:09:14 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -9,14 +9,14 @@ namespace CMP1903_A1_2324
     internal class Die
     {
         
-        private readonly Random _randomValue;
+        private readonly Random _randomValue;  //creates a object
         public Die()
         {
-            _randomValue = new Random();
+            _randomValue = new Random(); //sets to random
         }
         public int Roll()
         {  
-            return _randomValue.Next(1,7);
+            return _randomValue.Next(1,7); // creates a random number between 1 and 7
         }  
         
     }
